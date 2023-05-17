2124723736b2b49c03b04c76676eef32e52162e9
Ahmed Kosary <a7mdkosary@gmail.com>
2023-05-17 15:02:54 +0200

    add new frontend

--- Frontend/src/assets/js/script.js
+++ Frontend/src/assets/js/script.js
@@ -1,22 +1,21 @@
-(function($) {
-
+(function ($) {
   "use strict";
 
-  const tabs = document.querySelectorAll('[data-tab-target]')
-  const tabContents = document.querySelectorAll('[data-tab-content]')
+  const tabs = document.querySelectorAll("[data-tab-target]");
+  const tabContents = document.querySelectorAll("[data-tab-content]");
 
-  tabs.forEach(tab => {
-    tab.addEventListener('click', () => {
-      const target = document.querySelector(tab.dataset.tabTarget)
-      tabContents.forEach(tabContent => {
-        tabContent.classList.remove('active')
-      })
-      tabs.forEach(tab => {
-        tab.classList.remove('active')
-      })
-      tab.classList.add('active')
-      target.classList.add('active')
-    })
+  tabs.forEach((tab) => {
+    tab.addEventListener("click", () => {
+      const target = document.querySelector(tab.dataset.tabTarget);
+      tabContents.forEach((tabContent) => {
+        tabContent.classList.remove("active");
+      });
+      tabs.forEach((tab) => {
+        tab.classList.remove("active");
+      });
+      tab.classList.add("active");
+      target.classList.add("active");
+    });
   });
 
   // Responsive Navigation with Button
@@ -27,19 +26,18 @@
   hamburger.addEventListener("click", mobileMenu);
 
   function mobileMenu() {
-      hamburger.classList.toggle("active");
-      navMenu.classList.toggle("responsive");
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("responsive");
   }
 
   const navLink = document.querySelectorAll(".nav-link");
 
-  navLink.forEach(n => n.addEventListener("click", closeMenu));
+  navLink.forEach((n) => n.addEventListener("click", closeMenu));
 
   function closeMenu() {
-      hamburger.classList.remove("active");
-      navMenu.classList.remove("responsive");
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("responsive");
   }
-
 
   // close when click off of container
   /*$(document).on('click touchstart', function (e){
@@ -53,88 +51,86 @@
 
   });*/
 
-  $(document).ready(function(){
+  $(document).ready(function () {
+    Chocolat(document.querySelectorAll(".image-link"), {
+      imageSize: "contain",
+      loop: true,
+    });
 
-    Chocolat(document.querySelectorAll('.image-link'), {
-        imageSize: 'contain',
-        loop: true,
-    })
+    $("#header-wrap").on("click", ".search-toggle", function (e) {
+      var selector = $(this).data("selector");
 
-
-    $('#header-wrap').on('click', '.search-toggle', function(e) {
-      var selector = $(this).data('selector');
-
-      $(selector).toggleClass('show').find('.search-input').focus();
-      $(this).toggleClass('active');
+      $(selector).toggleClass("show").find(".search-input").focus();
+      $(this).toggleClass("active");
 
       e.preventDefault();
     });
 
-
     // close when click off of container
-    $(document).on('click touchstart', function (e){
-      if (!$(e.target).is('.search-toggle, .search-toggle *, #header-wrap, #header-wrap *')) {
-        $('.search-toggle').removeClass('active');
-        $('#header-wrap').removeClass('show');
+    $(document).on("click touchstart", function (e) {
+      if (
+        !$(e.target).is(
+          ".search-toggle, .search-toggle *, #header-wrap, #header-wrap *"
+        )
+      ) {
+        $(".search-toggle").removeClass("active");
+        $("#header-wrap").removeClass("show");
       }
     });
 
-    $('.main-slider').slick({
-        autoplay: false,
-        autoplaySpeed: 4000,
-        fade: true,
-        dots: true,
-        prevArrow: $('.prev'),
-        nextArrow: $('.next'),
-    }); 
+    $(".main-slider").slick({
+      autoplay: false,
+      autoplaySpeed: 4000,
+      fade: true,
+      dots: true,
+      prevArrow: $(".prev"),
+      nextArrow: $(".next"),
+    });
 
-    $('.product-grid').slick({
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        autoplay: false,
-        autoplaySpeed: 2000,
-        dots: true,
-        arrows: false,
-        responsive: [
-          {
-            breakpoint: 1400,
-            settings: {
-              slidesToShow: 3,
-              slidesToScroll: 1
-            }
+    $(".product-grid").slick({
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      autoplay: false,
+      autoplaySpeed: 2000,
+      dots: true,
+      arrows: false,
+      responsive: [
+        {
+          breakpoint: 1400,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
           },
-          {
-            breakpoint: 999,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 1
-            }
+        },
+        {
+          breakpoint: 999,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
           },
-          {
-            breakpoint: 660,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
-            }
-          }
-          // You can unslick at a given breakpoint now by adding:
-          // settings: "unslick"
-          // instead of a settings object
-        ]
+        },
+        {
+          breakpoint: 660,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          },
+        },
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ],
     });
 
     AOS.init({
       duration: 1200,
       once: true,
-    })
+    });
 
-    jQuery('.stellarnav').stellarNav({
-      theme: 'plain',
+    jQuery(".stellarnav").stellarNav({
+      theme: "plain",
       closingDelay: 250,
       // mobileMode: false,
     });
-
   }); // End of a document
-
-
 })(jQuery);
--- Frontend/src/components/component_example.jsx
+++ Frontend/src/components/component_example.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function component_example() {
-  return <div></div>;
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
 }
